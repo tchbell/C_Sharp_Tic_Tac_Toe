7e55c5bea05cd6f61dec3025b9193a69c11ba9d4
unknown <tchbell@gmail.com>
2021-10-16 15:39:30 -0400

    tic tac toe project C#

---
+++ README.md
@@ -0,0 +1,1 @@
+"# C_Sharp_Tic_Tac_Toe"  git init git add README.md git commit -m "first commit" git branch -M main git remote add origin git@github.com:tchbell/C_Sharp_Tic_Tac_Toe.git git push -u origin main
---
+++ Tic tac toe/MarkType.cs
@@ -0,0 +1,21 @@
+﻿namespace Tic_tac_toe
+{
+    /// <summary>
+    /// The type of value a cell in the game is currently at
+    /// </summary>
+public enum MarkType
+    {
+        /// <summary>
+        /// The cell hasn't been clicked yet
+        /// </summary>
+        Free,
+        /// <summary>
+        /// The cell is O
+        /// </summary>
+        Nought,
+        /// <summary>
+        /// The cell is X
+        /// </summary>
+        Cross
+    }
+}
